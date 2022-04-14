68158812f406bd35cf8ba2a00c52c4b153f72e9e
LucasOliveiraDantas <lucasdeoliveiradantas@protonmail.com>
2022-04-14 15:10:44 -0300

    ajustes

--- main.js
+++ main.js
@@ -25,3 +25,25 @@ function valor(tipo, valor) {
     document.getElementById('output-result').value += valor
   }
 }
+
+function ChangeT() {
+  const Bthemes = document.getElementById('th-switch').classList
+  const Mcolor = document.getElementById('main-color').classList
+
+  if (Bthemes.contains('th-1')) {
+    Bthemes.replace('th-1', 'th-2')
+    if (Bthemes.contains('th-2')) {
+      Mcolor.replace('th-s-1', 'th-s-2')
+    }
+  } else if (Bthemes.contains('th-2')) {
+    Bthemes.replace('th-2', 'th-3')
+    if (Bthemes.contains('th-3')) {
+      Mcolor.replace('th-s-2', 'th-s-3')
+    }
+  } else if (Bthemes.contains('th-3')) {
+    Bthemes.replace('th-3', 'th-1')
+    if (Bthemes.contains('th-1')) {
+      Mcolor.replace('th-s-3', 'th-s-1')
+    }
+  }
+}
